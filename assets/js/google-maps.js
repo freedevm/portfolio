75e7160f6e60fac24fe2a7f008a645fc4aa326f8
Daniyar <kimotohisao17@gmail.com>
2025-01-07 18:09:52 +0900

    updated 2

--- assets/js/google-maps.js
+++ assets/js/google-maps.js
@@ -1,7 +1,7 @@
 function initMap() {
     // Latitude and Longitude
     // var myLatLng = {lat: -6.207690, lng: 106.985270};
-    var myLatLng = {lat: 53.3996105, lng: -2.9806968};
+    var myLatLng = {lat: 35.17871, lng: 136.87694};
 
     var map = new google.maps.Map(document.getElementById('google-maps'), {
         zoom: 17,
@@ -11,6 +11,6 @@ function initMap() {
     var marker = new google.maps.Marker({
         position: myLatLng,
         map: map,
-        title: '78 Oak Avenue, Birmingham, B1 3CD, United Kingdom' // Title Location
+        title: ' Nakamura Ward, Nagoya, Aichi 453-0012, Japan' // Title Location
     });
 }
